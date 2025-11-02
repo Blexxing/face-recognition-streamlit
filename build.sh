4e4c75b07024184b3492b631247825f810f3812f
Blexxing <chiemeke.maduabu@gmail.com>
2025-11-02 23:51:25 +0000

    Added Procfile and build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y cmake
+
+# Update OS and install compilers and cmake
+apt-get update && apt-get install -y cmake build-essential
+
+# Upgrade pip to latest version
+pip install --upgrade pip
+
+# Install Python packages from requirements.txt
+pip install -r requirements.txt
