791d7b0a7b54aee1a09dd96bed7acaaee82c10ea
Blexxing <chiemeke.maduabu@gmail.com>
2025-11-03 00:16:52 +0000

    Added Procfile and build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Update OS and install compilers and cmake
-apt-get update && apt-get install -y cmake build-essential
-
 # Upgrade pip to latest version
 pip install --upgrade pip
 
-# Install prebuilt dlib wheel
-pip install "dlib==20.0.0" --find-links https://github.com/ageitgey/dlib/releases/tag/v20.0.0
-
-# Install other Python packages from requirements.txt (without dlib)
+# Install Python packages from requirements.txt
 pip install -r requirements.txt
